a66d966b43f1c4554470f7d5edcc20c1da51f6e1
YaroslavPshenichnikov <yaroslav.pshenichnikov@onlyoffice.com>
2023-08-01 16:19:19 +0300

    Initial commit

---
+++ origin-response-function/handler.js
@@ -0,0 +1,61 @@
+const path = require('path')
+const AWS = require('aws-sdk')
+
+const S3 = new AWS.S3({
+  signatureVersion: 'v4',
+})
+
+const Sharp = require('sharp')
+const BUCKET = 'bucket_placeholder'
+const QUALITY = 100
+
+exports.handler = async (event, context, callback) => {
+  const { request, response } = event.Records[0].cf
+  const { uri } = request
+  const headers = response.headers
+
+  if (path.extname(uri) === '.webp') {
+    console.log(response)
+    console.log(request)
+    if (parseInt(response.status) === 404) {
+      const format = request.headers['original-resource-type'] && request.headers['original-resource-type'][0]
+        ? request.headers['original-resource-type'][0].value.replace('image/', '')
+        : null
+
+      const key = uri.substring(1)
+      const s3key = key.replace('.webp', `.${format}`)
+      console.log("Printing key:", key)
+      console.log("Printing S3key", s3key)
+      try {
+        const bucketResource = await S3.getObject({ Bucket: BUCKET, Key: decodeURI(s3key) }).promise()
+        const sharpImageBuffer = await Sharp(bucketResource.Body)
+          .webp({ quality: +QUALITY })
+          .toBuffer()
+        console.log("Got the S3 image and converted. Trying to put it into S3")
+        await S3.putObject({
+          Body: sharpImageBuffer,
+          Bucket: BUCKET,
+          ContentType: 'image/webp',
+          CacheControl: 'max-age=31536000',
+          Key: decodeURI(key),
+          StorageClass: 'STANDARD'
+        }).promise()
+
+        response.status = 200
+        response.body = sharpImageBuffer.toString('base64')
+        response.bodyEncoding = 'base64'
+        response.headers['content-type'] = [{ key: 'Content-Type', value: 'image/webp' }]
+      } catch (error) {
+        console.log("Printing the error:")
+        console.error(error)
+      }
+    } else {
+      console.log("Its not 404, So returning webp")
+      headers['content-type'] = [{
+        'value': 'image/webp',
+        'key': 'Content-Type'
+      }]
+    }
+  }
+  callback(null, response)
+ }
---
+++ viewer-request-function/handler.js
@@ -0,0 +1,46 @@
+const userAgent = require('useragent')
+const path = require('path')
+
+exports.handler = (event, context, callback) => {
+  const request = event.Records[0].cf.request;
+  
+  request.uri = request.uri.replace(/^\/v9.5.0\/(en|ru|fr|de|es|pt|it|cs)\//,'/v9.5.0/');
+  request.uri = request.uri.replace(/^\/images\//,'/v9.5.0/images/');
+
+  const headers = request.headers;
+  const userAgentString = headers['user-agent'] && headers['user-agent'][0] ? headers['user-agent'][0].value : null;
+  const agent = userAgent.lookup(userAgentString);
+
+const browsersToInclude = [
+    { browser: 'Edge', version: 18 },
+    { browser: 'Firefox', version: 65 },
+    { browser: 'Chrome', version: 23 },
+    { browser: 'Opera', version: 13 },
+    { browser: 'Opera Mini', version: 1 },
+    { browser: 'Android', version: 53 },
+    { browser: 'Chrome Mobile', version: 55 },
+    { browser: 'Opera Mobile', version: 37 },
+    { browser: 'Baidu Browser', version: 7 },
+    { browser: 'QQ Browser', version: 1 },
+    { browser: 'UC Browser', version: 11 },
+    { browser: 'Samsung Internet', version: 4 }
+  ];
+
+  const supportingBrowser = browsersToInclude
+    .find(browser => browser.browser === agent.family && agent.major >= browser.version);
+    
+  if (supportingBrowser) {
+    const fileFormat = path.extname(request.uri).replace('.', '')
+    request.headers['original-resource-type'] = [{
+      key: 'Original-Resource-Type',
+      value: `image/${fileFormat}`
+    }];
+
+    const olduri = request.uri;
+    const newuri = olduri.replace(/(\.jpg|\.png|\.jpeg)$/g, '.webp');
+    request.uri = newuri;
+    console.log("newuri:" + newuri);
+  }
+
+  return callback(null, request)
+};
